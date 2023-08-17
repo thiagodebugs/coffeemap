66b3ff88f12af7bf963cb87797c9195dc1151178
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-08-17 14:56:26 -0400

    Pagina de Login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,56 @@
+import { Box, Button, Container, TextField, Link } from "@mui/material";
+import NextLink from "next/link";
+import { Footer, Logo } from "@/components";
+
 export default function Home() {
   return (
     <>
-      <h1>Bem Vindo ao CoffeMap!</h1>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Logo />
+        </Box>
+        <Container
+          component="form"
+          maxWidth="xs"
+          sx={{ display: "flex", flexDirection: "column" }}
+        >
+          <TextField
+            label="Usuário"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Senha"
+            variant="outlined"
+            fullWidth
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            variant="contained"
+            size="large"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Entrar
+          </Button>
+          <Link href="#" component={NextLink} variant="body2">
+            Esqueceu a senha?
+          </Link>
+        </Container>
+        <Box>
+          <Footer />
+        </Box>
+      </Box>
     </>
   );
 }
