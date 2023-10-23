28e38283a3754543158b85d1ef3619798fadb947
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-10-23 17:23:39 -0400

    test window is not defined

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -8,30 +8,32 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 
 export default function Map({ position, devices }) {
   return (
-    <MapContainer
-      center={position}
-      zoom={13}
-      style={{
-        height: "80vh",
-        width: "100%",
-      }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {devices.map((device, index) => (
-        <Marker key={index} position={[device.latitude, device.longitude]}>
-          <Popup>
-            <Typography variant="body1" fontWeight="bold">
-              {device.name}
-            </Typography>
-            <Typography variant="body1" margin="0">
-              {device.description}
-            </Typography>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <>
+      {/* <MapContainer
+        center={position}
+        zoom={13}
+        style={{
+          height: "80vh",
+          width: "100%",
+        }}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {devices.map((device, index) => (
+          <Marker key={index} position={[device.latitude, device.longitude]}>
+            <Popup>
+              <Typography variant="body1" fontWeight="bold">
+                {device.name}
+              </Typography>
+              <Typography variant="body1" margin="0">
+                {device.description}
+              </Typography>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer> */}
+    </>
   );
 }
