a5585bf50eedfd3b4918f99b4a6bc1b9fddf6907
Thiago Pereira Mendonca <thiagomendonca09@gmail.com>
2023-10-23 17:15:18 -0400

    test window is not defined
    change map to component

--- src/app/map/page.jsx
+++ src/app/map/page.jsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { Navbar } from "@/components";
-import { Box, Typography } from "@mui/material";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-// import "leaflet-defaulticon-compatibility";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import { Map, Navbar } from "@/components";
+import { Box } from "@mui/material";
+
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect, useState } from "react";
 
-export default function Map() {
+export default function MapPage() {
   const position = [-11.85385, -55.51208];
   const supabase = createClientComponentClient();
   const [devices, setDevices] = useState([]);
@@ -35,34 +32,7 @@ export default function Map() {
     <>
       <Navbar option="map">
         <Box>
-          {/* <MapContainer
-            center={position}
-            zoom={13}
-            style={{
-              height: "80vh",
-              width: "100%",
-            }}
-          >
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {devices.map((device, index) => (
-              <Marker
-                key={index}
-                position={[device.latitude, device.longitude]}
-              >
-                <Popup>
-                  <Typography variant="body1" fontWeight="bold">
-                    {device.name}
-                  </Typography>
-                  <Typography variant="body1" margin="0">
-                    {device.description}
-                  </Typography>
-                </Popup>
-              </Marker>
-            ))}
-          </MapContainer> */}
+          <Map position={position} devices={devices} />
         </Box>
       </Navbar>
     </>
---
+++ src/components/Map/index.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import Typography from "@mui/material/Typography";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+
+export default function Map({ position, devices }) {
+  return (
+    <MapContainer
+      center={position}
+      zoom={13}
+      style={{
+        height: "80vh",
+        width: "100%",
+      }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {devices.map((device, index) => (
+        <Marker key={index} position={[device.latitude, device.longitude]}>
+          <Popup>
+            <Typography variant="body1" fontWeight="bold">
+              {device.name}
+            </Typography>
+            <Typography variant="body1" margin="0">
+              {device.description}
+            </Typography>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+}
